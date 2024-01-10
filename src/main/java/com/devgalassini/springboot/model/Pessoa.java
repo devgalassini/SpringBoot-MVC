98a6b87ad4055d2c2ef6cedab458049d6204b03d
Priscila Galassini <priscilagalassini@gmail.com>
2024-01-10 08:36:18 -0300

    Iniciando o projeto

---
+++ src/main/java/com/devgalassini/springboot/model/Pessoa.java
@@ -0,0 +1,28 @@
+package com.devgalassini.springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Getter
+    @Setter
+    private String nome;
+    @Getter
+    @Setter
+    private String sobrenome;
+}
