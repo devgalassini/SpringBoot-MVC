917961602f7bdcbc03e343f21011ccac640cd12f
Priscila Galassini <priscilagalassini@gmail.com>
2024-01-11 09:26:05 -0300

    criando a classe Repository

--- src/main/java/com/devgalassini/springboot/controller/PessoaController.java
+++ src/main/java/com/devgalassini/springboot/controller/PessoaController.java
@@ -1,15 +1,26 @@
 package com.devgalassini.springboot.controller;
 
+import com.devgalassini.springboot.model.Pessoa;
+import com.devgalassini.springboot.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PessoaController {
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     @RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
     public String inicio() {
         return "cadastro/cadastropessoa";
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+    public String salvar(Pessoa pessoa){
+        pessoaRepository.save(pessoa);
+        return "cadastro/cadastropessoa";
+
+    }
 }
---
+++ src/main/java/com/devgalassini/springboot/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.devgalassini.springboot.repository;
+
+import com.devgalassini.springboot.model.Pessoa;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface PessoaRepository  extends CrudRepository <Pessoa, Long> {
+}
