9056ae2904ff2cb2766e9a6f0ef05c79c5a174fd
Priscila Galassini <priscilagalassini@gmail.com>
2024-01-11 08:57:13 -0300

    Iniciando a classe Controller

--- src/main/java/com/devgalassini/springboot/Application.java
+++ src/main/java/com/devgalassini/springboot/Application.java
@@ -3,8 +3,10 @@ package com.devgalassini.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.devgalassini.springboot.*"})
 @EntityScan(basePackages = "com.devgalassini.springboot.model")
 public class Application {
 
