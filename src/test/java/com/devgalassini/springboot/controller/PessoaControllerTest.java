3b0781f177f20bc3a336f097c8f5f1c07e651b98
Priscila Galassini <priscilagalassini@gmail.com>
2024-01-14 20:14:22 -0300

    Testes Unitários

---
+++ src/test/java/com/devgalassini/springboot/controller/PessoaControllerTest.java
@@ -0,0 +1,32 @@
+package com.devgalassini.springboot.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaControllerTest {
+
+    @Test
+    void inicio() {
+    }
+
+    @Test
+    void salvar() {
+    }
+
+    @Test
+    void pessoas() {
+    }
+
+    @Test
+    void editar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+
+    @Test
+    void pesquisar() {
+    }
+}
---
+++ src/test/java/com/devgalassini/springboot/repository/PessoaRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.devgalassini.springboot.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaRepositoryTest {
+
+    @Test
+    void findPessoaByName() {
+    }
+}
